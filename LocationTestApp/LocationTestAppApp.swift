a15ee6ee2a53382adf8724eb12080a64b7eaa793
f-sara <jyujyusa.68@gmail.com>
2023-09-26 09:10:51 +0900

    Initial Commit

---
+++ LocationTestApp/LocationTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocationTestAppApp.swift
+//  LocationTestApp
+//
+//  Created by 藤井紗良 on 2023/09/26.
+//
+
+import SwiftUI
+
+@main
+struct LocationTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
